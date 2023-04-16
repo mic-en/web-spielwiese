e4e4e3100e564dd7f3ce9224e10b45a020ce1b8b
Michael Engel <mail@engelmichael.de>
2023-04-16 17:47:31 +0200

    numberguessing game hinzugefügt

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,16 @@
-function updateTime() {let now = new Date();
-    document.getElementById("time").innerHTML = now;}
+
+
+function updateTime() {
+    let now = new Date();
+    document.getElementById("time").innerHTML = now;
+}
 
 setInterval(updateTime, 1000)
+
+let books = [
+	{ "author": "Jared Diamond", "title": "Arm und Reich", "pages": 980 },
+	{ "author" : "Barbara Tuchman", "title": "Der ferne Spiegel", "pages": 1020 },
+	{ "author" : "Frank Schätzing", "title": "Das unbekannte Universum", "pages": 1143 }
+];
+
+console.table (books);
